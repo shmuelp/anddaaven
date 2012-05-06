6e2420f131ca030869f8b4ff1a463195e0c2d47e
S Popper <shmuelp@gmail.com>
2012-05-06 01:08:33 -0400

    Fix Cupcake crashing bug.

--- AndDaaven/src/com/saraandshmuel/anddaaven/AndDaavenFactory.java
+++ AndDaaven/src/com/saraandshmuel/anddaaven/AndDaavenFactory.java
@@ -2,29 +2,27 @@ package com.saraandshmuel.anddaaven;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 
 public class AndDaavenFactory {
 	
 	public static Intent getTefillaIntent(Context context) {
-		Intent result;
+		Intent result = new Intent();
+		String pkg = context.getPackageName(); // com.saraandshmuel.anddaaven
 		
 		// Ice Cream Sandwich
 		if (AndDaavenModel.getAndroidSdkVersion() >= 14 ) { 
-//			Toast.makeText(context, "returning ICS intent", Toast.LENGTH_SHORT).show();
-			result = new Intent(context, com.saraandshmuel.anddaaven.AndDaavenTefillaIcs.class);
+			result.setClassName(pkg, "com.saraandshmuel.anddaaven.AndDaavenTefillaIcs");
 		// Honeycomb
-		} else if (AndDaavenModel.getAndroidSdkVersion() >= 11 ) { 
-//			Toast.makeText(context, "returning Honeycombd intent", Toast.LENGTH_SHORT).show();
-			result = new Intent(context, com.saraandshmuel.anddaaven.AndDaavenTefillaHoneycomb.class);
+		} else if (AndDaavenModel.getAndroidSdkVersion() >= 11 ) {
+			result.setClassName(pkg, "com.saraandshmuel.anddaaven.AndDaavenTefillaHoneycomb.class");
 		// Froyo
 		} else if (AndDaavenModel.getAndroidSdkVersion() >= 8 ) { 
-//			Toast.makeText(context, "returning Froyo intent", Toast.LENGTH_SHORT).show();
-			result = new Intent(context, com.saraandshmuel.anddaaven.AndDaavenTefillaFroyo.class);
+			result.setClassName(pkg, "com.saraandshmuel.anddaaven.AndDaavenTefillaFroyo");
 		// Cupcake
 		} else {
-//			Toast.makeText(context, "Returning normal intent", Toast.LENGTH_SHORT).show();
-			result = new Intent(context, com.saraandshmuel.anddaaven.AndDaavenTefilla.class);
+			result.setClassName(pkg, "com.saraandshmuel.anddaaven.AndDaavenTefilla");
 		}
 		
 		return result;
