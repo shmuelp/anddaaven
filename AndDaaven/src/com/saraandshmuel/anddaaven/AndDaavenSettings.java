911bf8a7306e2e6ecba9804349825bf091b55bbc
S Popper <shmuelp@gmail.com>
2012-05-07 22:33:43 -0400

    Show current font size in font picker.

--- AndDaaven/src/com/saraandshmuel/anddaaven/AndDaavenSettings.java
+++ AndDaaven/src/com/saraandshmuel/anddaaven/AndDaavenSettings.java
@@ -2,6 +2,8 @@ package com.saraandshmuel.anddaaven;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -18,10 +20,32 @@ public class AndDaavenSettings extends PreferenceActivity {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
 		
-		SharedPreferences prefs=PreferenceManager.getDefaultSharedPreferences(this);
+        PreferenceManager man=getPreferenceManager();
+        
+        SharedPreferences prefs=PreferenceManager.getDefaultSharedPreferences(this);
 		if (prefs.getBoolean("test.autoIndex", false)) {
-			PreferenceManager man=getPreferenceManager();
 			man.findPreference("AutoIndexBrachot").setEnabled(true);
+		}
+		
+		Preference pref=man.findPreference("FontSize");
+		if (pref != null && pref instanceof ListPreference) {
+			Log.v(TAG, "Found font size pref");
+			ListPreference listPref = (ListPreference) pref;
+			CharSequence[] entries = listPref.getEntries();
+			CharSequence[] values = listPref.getEntryValues();
+			CharSequence[] newEntries = new CharSequence[entries.length+1];
+			CharSequence[] newValues = new CharSequence[values.length+1];
+			System.arraycopy(entries, 0, newEntries, 1, entries.length);
+			System.arraycopy(values, 0, newValues, 1, values.length);
+			//TODO L10N
+			String curValue = prefs.getString("FontSize", "20");
+			Log.v(TAG, "Adding new pref for value " + curValue);
+			newValues[0] = curValue;
+			newEntries[0] = "Current ("+curValue+")";
+			Log.v(TAG, "Setting new values");
+			listPref.setEntries(newEntries);
+			listPref.setEntryValues(newValues);
+			Log.v(TAG, "Finished modifying values");
 		}
 		
 		Log.v(TAG, "onCreate() ending");
